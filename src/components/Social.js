9d06dc683cfb78c11b7a2a7b047698e573429aa7
Thomas Edgeworth <tom.edgeworth47@gmail.com>
2020-02-05 10:52:36 -0500

    Updated twitter link to update when quote updates

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
-import jsonData from "./quotes.json";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-const Social = styled.div`
+const SocialWrapper = styled.div`
   width: 15%;
   display: flex;
   flex-direction: row;
@@ -18,38 +17,78 @@ const Social = styled.div`
   // border-color: red;
 `;
 
-let dailyQuote = jsonData[0];
-console.log(dailyQuote.quote);
-console.log(dailyQuote.author);
+class Social extends Component {
+  state = {
+    daily_quote: [],
+    encoded_quote: []
+  };
 
-let quoteAndAuthor = `${dailyQuote.quote}" -${dailyQuote.author}`;
+  async componentDidMount() {
+    const daily_quote = await this.loadData();
+    this.setState({
+      daily_quote
+    });
+  }
 
-function encode(thequote) {
-  var unencoded = quoteAndAuthor;
-  let encoded = encodeURIComponent(unencoded)
-    .replace(/'/g, "%27")
-    .replace(/"/g, "%22");
-  return encoded;
+  async loadData() {
+    let initialDate = new Date(2020, 0, 28);
+    let now = Date.now();
+    let difference = now - initialDate;
+    let millisecondsPerDay = 24 * 60 * 60 * 1000;
+    let daysSince = Math.floor(difference / millisecondsPerDay);
+    console.log("daysSince", daysSince);
+
+    const url = `http://localhost:3001/v1/post/${daysSince}`;
+    const response = await fetch(url);
+    const data = response.json();
+    console.log(data);
+    data.then(response => {
+      this.setState(
+        {
+          encoded_quote: response
+        },
+        () => console.log(this.state)
+      );
+    });
+  }
+
+  encodeQuote() {
+    let quoteAndAuthor = `${this.state.encoded_quote.quote} -${this.state.encoded_quote.author}`;
+
+    function encode(thequote) {
+      var unencoded = quoteAndAuthor;
+      let encoded = encodeURIComponent(unencoded)
+        .replace(/'/g, "%27")
+        .replace(/"/g, "%22");
+      return encoded;
+    }
+
+    let twitterFill = encode();
+
+    let twitterReference = `https://twitter.com/intent/tweet?text=${twitterFill}`;
+    console.log(twitterReference);
+    return twitterReference;
+  }
+
+  // TODO edit this when we have website
+  // let facebookReference =
+  //   "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2Fshare-button%2F&amp;src=sdkpreparse";
+
+  render() {
+    const { daily_quote } = this.state;
+    let twitterLink = this.encodeQuote();
+    console.log(this.state);
+    return (
+      <SocialWrapper>
+        <a className="facebook-share-button" href={daily_quote}>
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a className="twitter-share-button" href={twitterLink}>
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </SocialWrapper>
+    );
+  }
 }
 
-let twitterFill = encode();
-
-let twitterReference = `https://twitter.com/intent/tweet?text="${twitterFill}`;
-console.log(twitterReference);
-
-// TODO edit this when we have website
-let facebookReference =
-  "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2Fshare-button%2F&amp;src=sdkpreparse";
-
-export default () => (
-  <>
-    <Social>
-      <a className="facebook-share-button" href={facebookReference}>
-        <FontAwesomeIcon icon={faFacebook} />
-      </a>
-      <a className="twitter-share-button" href={twitterReference}>
-        <FontAwesomeIcon icon={faTwitter} />
-      </a>
-    </Social>
-  </>
-);
+export default Social;
